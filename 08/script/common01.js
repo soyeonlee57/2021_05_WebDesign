9185383b732cb204e206dfa4c6fc37dcf776ed28
soyeonlee <soyeonlee57@gmail.com>
2021-05-23 11:35:32 +0900

    팝업

---
+++ 08/script/common01.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".layerpopup").click(function(){
+       $(".layer").show(); 
+       $(".layer-bg").show(); 
+    });
+     $(".close-btn").click(function(){
+       $(".layer").hide(); 
+       $(".layer-bg").hide(); 
+    });
+});
